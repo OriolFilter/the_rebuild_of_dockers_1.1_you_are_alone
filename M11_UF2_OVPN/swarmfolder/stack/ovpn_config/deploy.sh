e2f99d4cd4075f2d28bcf0392c9685c826fad8f0
oriolfilter <filter.oriol@gmail.com>
2021-06-04 23:34:57 +0200

    D22-21 Started OVPN

---
+++ M11_UF2_OVPN/swarmfolder/stack/ovpn_config/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Deploy
+STACK="ovpn_home"
+#sudo docker stack rm "${STACK}"
+#sudo docker stack deploy -c docker-compose.yaml "$STACK"
+#sudo docker service update --force "${STACK}_ovpn"
+docker-compose up -d
+
